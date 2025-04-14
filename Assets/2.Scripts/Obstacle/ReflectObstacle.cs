a282ee3f2f450072435db91a48556045492e33f4
royun0307 <109058557+royun0307@users.noreply.github.com>
2025-04-14 16:03:41 +0900

    Feat : 반사 장애물의 반사 속도 메커니즘 변경
    반사 장애물의 반사 속도가 타겟의 충돌 속도와 비례

--- Assets/2.Scripts/Obstacle/ReflectObstacle.cs
+++ Assets/2.Scripts/Obstacle/ReflectObstacle.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 public class ReflectObstacle : MonoBehaviour
 {
     [SerializeField] private float _reflectPower = -1f;
+    [SerializeField] private float _reflectMinPower = -1f;
+    [SerializeField] private float _reflectMaxPower = -1f;
+
     private GameObject target;
 
     private void Start()
     {
         var data = ObstacleManager.Instance.obstacleData;
         Utilitys.SetIfNegative(ref _reflectPower, data.reflectPower);
+        Utilitys.SetIfNegative(ref _reflectMinPower, data.reflectMinPower);
+        Utilitys.SetIfNegative(ref _reflectMaxPower, data.reflectMaxPower);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Reflect(collision.gameObject);
+            Reflect(collision.gameObject, collision.relativeVelocity.magnitude);
         }
     }
 
-    private void Reflect(GameObject target)
+    private void Reflect(GameObject target, float impactSpeed)
     {
         Vector3 direction = target.transform.position - transform.position;
         direction.y = 0f;
@@ -31,12 +36,14 @@ public class ReflectObstacle : MonoBehaviour
         if (target.TryGetComponent(out Player player))
         {
             ObstacleManager.Instance.StartLockMovement(player);
+        }
 
-            if (target.TryGetComponent(out Rigidbody rb))
-            {
-                rb.velocity = Vector3.zero;
-                rb.AddForce(direction * _reflectPower, ForceMode.Impulse);
-            }
+        if (target.TryGetComponent(out Rigidbody rb))
+        {
+            rb.velocity = Vector3.zero;
+
+            float scaledPower = Mathf.Clamp(impactSpeed * _reflectPower, _reflectMinPower, _reflectMaxPower);
+            rb.AddForce(direction * scaledPower, ForceMode.Impulse);
         }
     }
 }
